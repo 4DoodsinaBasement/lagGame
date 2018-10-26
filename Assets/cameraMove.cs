4632d074918b4e81ff7390a7f48d53c6d94ef3e3
Araliatrix <agentisaacson@gmail.com>
2018-10-26 17:25:25 -0600

    added player movement boundaries

---
+++ Assets/cameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour 
+{
+	public float cameraSpeed = 1f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.Translate(cameraSpeed * 0.01f, 0, 0);
+	}
+}
