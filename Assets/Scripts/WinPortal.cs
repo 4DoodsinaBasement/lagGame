9bb735e64c5c1023f07fca1ccd3803eb7f8fd6a6
jt <j4c0bh3rrm4nn@gmail.com>
2018-10-27 15:45:33 -0700

    added moveBlock and winPortal

---
+++ Assets/Scripts/WinPortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinPortal : MonoBehaviour {
+
+    [SerializeField] private string scene = "Animation_Scene";
+    [SerializeField] private string tag = "winPortal";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == tag)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
